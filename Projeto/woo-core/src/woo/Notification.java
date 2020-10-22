2e7e7522a90606bb5e11ef4965be404036bd3302
FunkyCracky <futebol4@hotmail.com>
2020-10-22 22:05:11 +0100

    Notifications!

--- Projeto/woo-core/src/woo/Notification.java
+++ Projeto/woo-core/src/woo/Notification.java
@@ -1,9 +1,11 @@
 public class Notification {
     
-    private int _productID;
+    private String _type;
+    private String _productID;
     private int _productPrice;
 
-    public Notification(int pID, int pPrice){
+    public Notification(String type, String pID, int pPrice){
+        _type = type;
         _productID = pID;
         _productPrice = pPrice;
     }
